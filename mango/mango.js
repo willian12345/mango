c4bf073ff92f9b270c4579adde3aa49af3cd42eb
willian <willian12345@126.com>
2013-07-15 17:10:45 +0800

    增加更多api
    增加更多api

--- mango/mango.js
+++ mango/mango.js
@@ -1,15 +1,72 @@
+/*!
+ * mango JavaScript Library v1.0
+ * author: willian.xiaodong
+ * Date: 2013-06-15
+ */
 ;(function(window){
-    _mango = {};
-    var objectTypes = ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", 'Function'];
-    for(var i = 0, c; c = objectTypes[i++];){
-        _mango['is'+c] = (function(type){
-            return function(obj){
-                if(!obj) return false;
-                return Object.prototype.toString.call(obj) == "[object " + type + "]";
-            }
-        })(c);
-    }
-    var Mango = function (selector, context) {
+    var Mango, mango, _mango = {}, Events = {},EventsGuid=0, EventTrigger, _$, _extend;
+
+    _$ = (function(){
+        // create functions which is object's type check
+        var _o = {};
+        var o = ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", 'Function'];
+        for(var i = 0, c; c = o[i++];){
+            _o['is'+c] = (function(type){
+                return function(obj){
+                    if(!obj) return false;
+                    return Object.prototype.toString.call(obj) == "[object " + type + "]";
+                }
+            })(c);
+        }
+        return _o;
+    })();
+    // object extend function
+    _extend = function() {
+        // copy reference to target object
+        var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, options;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+            target = arguments[1] || {};
+            // skip the boolean and the target
+            i = 2;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !_$.isFunction(target) )
+            target = {};
+
+        for ( ; i < length; i++ )
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null )
+                // Extend the base object
+                for ( var name in options ) {
+                    var src = target[ name ], copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy )
+                        continue;
+
+                    // Recurse if we're merging object values
+                    if ( deep && copy && typeof copy === "object" && !copy.nodeType )
+                        target[ name ] = _extend( deep, 
+                            // Never move original objects, clone them
+                            src || ( copy.length != null ? [ ] : { } )
+                        , copy );
+
+                    // Don't bring in undefined values
+                    else if ( copy !== undefined )
+                        target[ name ] = copy;
+
+                }
+
+        // Return the modified object
+        return target;
+    }; 
+
+    // main Function 
+    Mango = function (selector, context) {
         context = context || document;
         if(!selector) return null;
         if(selector.context){// mango 直接返回
@@ -19,7 +76,7 @@
             this.length = 1;
             this.context = selector;
             return this;
-        }else if(_mango.isString(selector)){
+        }else if(_$.isString(selector)){
             this.length = 0;
             var matched = [];
             selector.split(',').forEach(function(v,i){
@@ -27,6 +84,12 @@
                 if(a === '#'){
                     matched.push(context.querySelector(v))
                 }else if(a === '.'){
+                    Mango.each(context.querySelectorAll(v),function(ele, i){
+                        this[this.length] = ele;
+                        this.length++;
+                        // console.log(this, ele, i)
+                    }.bind(this));
+                }else{
                     Mango.each(context.querySelectorAll(v),function(ele, i){
                         this[this.length] = ele;
                         this.length++;
@@ -40,19 +103,18 @@
             }
             this.context = context;
             return this;
-        }else if(_mango.isArray(selector)){
+        }else if(_$.isArray(selector)){
             selector.forEach(function (v, i) {
                 this[i] = v;
             }.bind(this));
             this.length = selector.length;
             this.context = context;
             return this;
-        }else if(_mango.isFunction(selector)){
+        }else if(_$.isFunction(selector)){
             window.document.addEventListener('DOMContentLoaded', function () {
                 selector();
             }, false)
         }
-
     };
     Mango.each = function (object, callback) {
         for(var i=0,j=object.length; i<j; i++){
@@ -60,21 +122,12 @@
         }
     };
     Mango.prev = function (node) {
-        node = node.previousSibling;
-        while(node.nodeType!=1){
-            node=node.previousSibling;
-            if(!node) return null;
-        };
-        return node;
+        return node.previousElementSibling;
     };
     Mango.next = function (node) {
-        node = node.nextSibling;
-        while(node.nodeType!=1){
-            node=node.nextSibling;
-            if(!node) return null;
-        };
-        return node;
+        return node.nextElementSibling;
     };
+    
     Mango.prototype = {
         constructor: Mango
         ,find: function (query) {
@@ -171,9 +224,15 @@
                 }
                 return this;
             }
+            if(_$.isObject(name)){
+                for(var v in name){
+                    $(this).prop(v, name[v]);
+                }
+                return this;
+            }
             if(value !== undefined){
                 Mango.each(this, function (node) {
-                    node[name] = value
+                    node[name] = value;
                 });
                 return this;
             }else{
@@ -184,6 +243,12 @@
             if(!this[0]){
                 if(value === undefined){
                     return undefined;
+                }
+                return this;
+            }
+            if(_$.isObject(name)){
+                for(var v in name){
+                    $(this).attr(v, name[v]);
                 }
                 return this;
             }
@@ -203,7 +268,12 @@
                 }
                 return this;
             }
-
+            if(_$.isObject(name)){
+                for(var v in name){
+                    $(this).data(v, name[v]);
+                }
+                return this;
+            }
             if(value !== undefined){
                 Mango.each(this, function (node) {
                     node.dataset[name] = value;
@@ -213,72 +283,241 @@
                 return this[0].dataset[name];
             }
         }
+        ,on: function (eventName, selector, cb) {
+            var _cb = cb, eventDispacher;
+            
+            if(!eventName) return this;
+
+            if(arguments.length === 2){
+                _cb = selector;
+                selector = null;
+            }
+            Mango.each(this, function (el) {
+                // The private attribute '_mangoeventid'
+                // is make relationship between dom and Events object
+                var _id = el._mangoeventid; 
+                if(!_id){
+                    _id = EventsGuid++;
+                    el._mangoeventid =  _id;
+                    Events[_id] = {};
+                }
+                if(!Events[_id][eventName]){
+                    Events[_id][eventName] = {
+                        type: eventName
+                        ,handles: []
+                    };
+                }
+
+                window.Events = Events;///
+
+                var handle = function (e) {
+                    var arr, tagName, className, _el, _selector, currentTarget;
+                    _el = e.srcElement;
+                    // check event namespace
+                    if(e._privateEvent && e._privateEvent != eventName){
+                        return ;
+                    }
+                    if(selector){
+                        if(el.contains(_el)){ // check delegate element
+                            _cb.call(_el, e);
+                        }
+                    }else{
+                        _cb.call(el, e);
+                    }
+                }
+                // Add eventListener without event namespace
+                el.addEventListener(eventName.split('/')[0], handle, false);
+
+                // push handle to Events for removeEventListener later
+                Events[_id][eventName].handles.push(handle);
+            });
+
+            return this;
+        }
+        ,off: function (eventName) {
+            if(!eventName) return this;
+            Mango.each(this, function(el){
+                var _id = el._mangoeventid;
+                if(_id){
+                    Events[_id][eventName].handles.forEach(function(handle){
+                        el.removeEventListener(eventName.split('/')[0], handle, false);
+                    });
+                    Events[_id][eventName].handles.length = 0;
+                }
+            });
+        }
+        ,trigger: function (eventName) {
+            var privateEvent = null, eventNamespace;
+            if(!eventName) return this;
+
+            eventNamespace = eventName.split('/');
+            if(eventNamespace.length > 1){
+                privateEvent = eventName;
+            }
+            Mango.each(this, function(el) {
+                EventTrigger.trigger(el,eventNamespace[0], privateEvent);
+            });
+            return this;
+        }
+        ,offset: function () {
+            if(!this[0]){
+                return {left:0, top:0};
+            }
+            return {left:this[0].offsetLeft, top:this[0].offsetTop};
+        }
+        ,css: function (p, v) {
+            var s, i;
+            if(!this[0]){
+                if(v === undefined){
+                    return undefined;
+                }
+                return this;
+            }
+            if(_$.isObject(p)){
+                for(var _v in p){
+                    $(this).css(_v, p[_v]);
+                }
+                return this;
+            }
+            if(v !== undefined){
+                var strArr = [];
+                // Uppercase the letter after the '-'
+                p.split('-').forEach(function(str){
+                    if(str.length)
+                        strArr.push(str.substr(0,1).toUpperCase() + str.substr(1));
+                });
+                p = strArr.join('');
+                // lowercase the first letter
+                p = p.substr(0,1).toLowerCase() + p.substr(1);
+                if(!/em|px|%/.test(v)){
+                    v += 'px';
+                }
+                Mango.each(this, function (node) {
+                    node.style[p] = v;
+                });
+                return this;
+            }else{
+                s = window.getComputedStyle(this[0]);
+                if(s){
+                    return s[p];
+                }
+                return undefined;
+                
+            }
+            return this;
+        }
     };
-    var mango = (function(){
+
+    // extend width,height,innerWidth, outerWidth, innerHeight, outerHeight
+    ['Width', 'Height'].forEach(function(v) {
+        _v = v.toLowerCase();
+        Mango.prototype[_v] = function (value) {
+            var re;
+            if(value===undefined){
+                if(!this[0]) return undefined;
+                re = window.getComputedStyle(this[0]);
+                if(re){
+                    return parseInt(re[_v]);
+                }
+                // return this[0]['client' + v];
+            }else{
+                if(typeof value === 'string'){
+                    if(!/em|px|%/.test(value)){
+                        value += 'px';
+                    }
+                }
+                Mango.each(this, function(el){
+                    el.style[_v] = value;
+                });
+            }
+            return this;
+        }
+        // include padding+border
+        Mango.prototype['inner' + v] = function (value) {
+            if(value === undefined){
+                if(!this[0]) return undefined;
+                return this[0]['offset' + v];
+            }
+            return this;
+        }
+        // include padding+border+margin
+        Mango.prototype['outer' + v] = function (value) {
+            var s, mtb, mlr;
+            if(value===undefined){
+                if(!this[0]) return undefined;
+                s = window.getComputedStyle(this[0]);
+                mtb = parseInt(s['margin-top']) + parseInt(s['margin-bottom']);
+                mlr = parseInt(s['margin-left']) + parseInt(s['margin-right']);
+                return this[0]['offset' + v] + ((v==='Width') ? mlr : mtb);
+            }
+            return this;
+        }
+    });
+
+    // w3c original event model
+    EventTrigger = {
+        trigger: function (element, eventName, _privateEvent){
+            var options = _extend(EventTrigger.defaultOptions, arguments[2] || {});
+            var oEvent, eventType = null;
+
+            for (var name in EventTrigger.eventMatchers){
+                if (EventTrigger.eventMatchers[name].test(eventName)) { eventType = name; break; }
+            }
+
+            if (!eventType)
+                throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+            if (document.createEvent){
+                oEvent = document.createEvent(eventType);
+                oEvent._privateEvent = _privateEvent;
+                if (eventType == 'HTMLEvents'){
+                    oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+                }
+                else
+                {
+                    oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+                    options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+                    options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+                }
+                element.dispatchEvent(oEvent);
+            }
+            return element;
+        }
+        ,eventMatchers: {
+            'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+            'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+        }
+        ,defaultOptions: {
+            pointerX: 0,
+            pointerY: 0,
+            button: 0,
+            ctrlKey: false,
+            altKey: false,
+            shiftKey: false,
+            metaKey: false,
+            bubbles: true,
+            cancelable: true
+        }
+    }
+
+    // main function for
+    mango = (function(){
         return function (selector, context) {
             return new Mango(selector, context);
         }
     })();
-    mango.extend = function() {
-        // copy reference to target object
-        var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, options;
 
-        // Handle a deep copy situation
-        if ( typeof target === "boolean" ) {
-            deep = target;
-            target = arguments[1] || {};
-            // skip the boolean and the target
-            i = 2;
-        }
-
-        // Handle case when target is a string or something (possible in deep copy)
-        if ( typeof target !== "object" && !_mango.isFunction(target) )
-            target = {};
-
-        // extend jQuery itself if only one argument is passed
-        if ( length == i ) {
-            target = this;
-            --i;
-        }
-
-        for ( ; i < length; i++ )
-            // Only deal with non-null/undefined values
-            if ( (options = arguments[ i ]) != null )
-                // Extend the base object
-                for ( var name in options ) {
-                    var src = target[ name ], copy = options[ name ];
-
-                    // Prevent never-ending loop
-                    if ( target === copy )
-                        continue;
-
-                    // Recurse if we're merging object values
-                    if ( deep && copy && typeof copy === "object" && !copy.nodeType )
-                        target[ name ] = mango.extend( deep, 
-                            // Never move original objects, clone them
-                            src || ( copy.length != null ? [ ] : { } )
-                        , copy );
-
-                    // Don't bring in undefined values
-                    else if ( copy !== undefined )
-                        target[ name ] = copy;
-
-                }
-
-        // Return the modified object
-        return target;
-    }; 
-    mango.extend(mango, _mango);
+    // merge into mango
+    _extend(mango, _$);
+    mango.extend = _extend;
     mango.param = function( a ) {
         var s = [ ];
-
         function add( key, value ){
             s[ s.length ] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
         };
-
         // If an array was passed in, assume that it is an array
         // of form elements
-        if ( mango.isArray(a) || a.jquery )
+        if ( mango.isArray(a))
             // Serialize the form elements
             Mango.each( a, function(){
                 add( this.name, this.value );
@@ -309,7 +548,7 @@
         var success = opts.success;
 
         request.onreadystatechange = function() {
-            //判断是否发送成功，是否找到请求页面等 
+            // Is send success or get the page success
             if (request.readyState==4 && request.status==200) {
                 if(/json/g.test(request.getResponseHeader("Content-Type"))){
                     if(mango.isFunction(success)){
